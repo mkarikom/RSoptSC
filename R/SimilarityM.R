4b8d21dc63cd157d28195dd0404ffe8d64e38d5e
Matt Karikomi <mattkarikomi@gmail.com>
2019-03-07 19:10:14 -0800

    performance improvement to computeM, recompressed Joost data

--- R/SimilarityM.R
+++ R/SimilarityM.R
@@ -14,40 +14,42 @@
 #'
 #' @export
 #'
-SimilarityM <- function(lambda,data){
 
+SimilarityM <- function(lambda,data, ...){
+  
+  # make min = 0, max = 1, then divide by column vector norm
   m = nrow(data)
   n = ncol(data)
-
+  
   X <- data - min(data)
   X = X / max(X)
-
+  
   for(i in 1:n){
     X[,i] <- X[,i] / norm(X[,i], "2")
   }
-
+  
   if (m >= 60){
-
+    
     pca_data = prcomp(t(X))
     coeff1 <- pca_data$rotation[,1:60]
     X1 <- pca_data$x[,1:60]
     pca_eigvalue1 <- pca_data$sdev
-
+    
   } else {
     pca_data = prcomp(t(X))
     coeff1 <- pca_data$rotation[,1:m]
     X1 <- pca_data$X1[,1:m]
     pca_eigvalue1 <- pca_data$sdev
   }
-
+  
   eigengaps = abs(pca_eigvalue1[2:(length(pca_eigvalue1)-1)] - pca_eigvalue1[-(1:2)])
   No_Comps1 = which(max(eigengaps)==eigengaps)
-
+  
   cc = cumsum(pca_eigvalue1[-(1)])
   dd = cc[-(1)]/sum(pca_eigvalue1[-(1)])
-
+  
   K1 = length(which(dd<=0.3))
-
+  
   if (K1 <= 10){
     K = 10
   } else if (K1 >=30){
@@ -60,20 +62,19 @@ SimilarityM <- function(lambda,data){
   # InitY = InitY_data$x[,1:dim_init]
   # Currently this is not "Standardize" as in MATLAB tsne('Standardize', TRUE)
   # Currently this is not "InitialY" as in MATLAB tsne('InitialY', InitY)
-
+  
   set.seed(1)
-  X2 <- Rtsne(X = t(X), perplexity = 20, pca_center = TRUE, pca_scale = TRUE, dims = 3)
+  X2 <- Rtsne(X = t(X), ...)
   D <- matrix(1, n, n)
   if (No_Comps1>=1){
     No_Comps1 = 1
   }
   knn_object = get.knnx(X2$Y[,1:(No_Comps1+2)], X2$Y[,1:(No_Comps1+2)], k = K)
   IDX = knn_object$nn.index
-
+  
   for (jj in 1:n){
     D[jj,IDX[jj,]] = 0
   }
-
   Z <- computeM(D,X,lambda)
   Z$Z[Z$Z <= .Machine$double.eps] <- 0
   W <- 0.5*(abs(Z$Z)+abs(t(Z$Z)))
@@ -99,75 +100,84 @@ SimilarityM <- function(lambda,data){
 computeM <- function(D,X,lambda){
   ## ADMM iteration
   maxiter = 100
-  Err = matrix(0, maxiter, 2)
-  rho = 5        # 5
-  mu = 10^(-6)   # 10^(-6)
-  mumax = 10^(6)
-  epsilon = 10^(-5)
-  m = nrow(X)
-  n = ncol(X)
-  Z = matrix(0,n,n)
-  E = matrix(0, m, n)
-  Y1 = matrix(0, m, n)
-  Y2 = matrix(0, 1, n)
-  Y3 = matrix(0, n, n)
-  iter = 0
-
+  Err <- matrix(0, maxiter, 2)
+  rho <- 5        # 5
+  mu <- 10^(-6)   # 10^(-6)
+  mumax <- 10^(6)
+  epsilon <- 10^(-5)
+  m <- nrow(X)
+  n <- ncol(X)
+  Z <- matrix(0,n,n)
+  E <- matrix(0, m, n)
+  Y1 <- matrix(0, m, n)
+  Y2 <- matrix(0, 1, n)
+  Y3 <- matrix(0, n, n)
+  iter <- 0
+  XXZ <- X-X%*%Z
+  
   while(TRUE){
-    iter = iter + 1
+    iter <- iter + 1
     if (iter >= maxiter){
       print("reached max iter")
-      return(list(Z = Z, E = norm(X-X%*%Z,"f")))
+      return(list(Z = Z, E = norm(XXZ,"f")))
     }
-
-
+    
+    
     # step 1: Update J
-    mu = min(rho*mu,mumax)
-    svd_data = svd(Z-Y3/mu, nu = nrow(Z-Y3/mu), nv = ncol(Z-Y3/mu))
-    U = svd_data$u
-    S = diag(svd_data$d, nrow=nrow(Z), ncol=ncol(Z))
-    V = svd_data$v
-
-    R = length(diag(S))
-    Dmu = matrix(0, R, R)
-
-    MM = pmax(diag(S)-(1/mu)*matrix(1, R, 1), matrix(0, R,1))
+    mu <- min(rho*mu,mumax)
+    svd_data <- svd(Z-Y3/mu, nu = nrow(Z-Y3/mu), nv = ncol(Z-Y3/mu))
+    U <- svd_data$u
+    S <- diag(svd_data$d, nrow=nrow(Z), ncol=ncol(Z))
+    V <- svd_data$v
+    
+    R <- length(diag(S))
+    Dmu <- matrix(0, R, R)
+    
+    MM <- pmax(diag(S)-(1/mu)*matrix(1, R, 1), matrix(0, R,1))
     for (i in 1:R){
-      Dmu[i,i] = MM[i]
+      Dmu[i,i] <- MM[i]
     }
-
-    J = U%*%Dmu%*%t(V)
-
+    
+    J <- U%*%Dmu%*%t(V)
+    
     if (iter >= 3){
-      if (Err[iter-1,1] >= Err[iter-2,1] || norm(as.matrix(X-X%*%Z),"2") <= epsilon){
+      if (Err[iter-1,1] >= Err[iter-2,1] || norm(as.matrix(XXZ),"2") <= epsilon){
         print("convergence by error min")
-        return(list(Z = Z, E = norm(X-X%*%Z,"f")))
+        return(list(Z = Z, E = norm(XXZ,"f")))
       }
     }
     # Update E
-    QQ = X-X%*%Z+Y1/mu
-    for (j in 1:n){
-      if (sqrt(sum(QQ[,j]^2)) > lambda/mu){
-        E[,j] = (sqrt(sum(QQ[,j]^2))-lambda/mu)/(sqrt(sum(QQ[,j]^2)))*as.matrix(QQ[,j])
-      } else {
-        E[,j] = matrix(0,length(QQ[,j]))
+    QQ <- XXZ+Y1/mu
+    E <- apply(QQ, 2, function(x){
+      normX <- norm(as.matrix(x), type = '2')
+      as.vector(((normX - lambda/mu) / normX)) * as.matrix(x)
+    })
+    normsQQ <- apply(QQ, 2, function(x){
+      norm(x, type = '2')
+    })
+    for(j in 1:n){
+      if(normsQQ[j] <= lambda/mu){
+        E[,j] <- matrix(0,length(QQ[,j]))
       }
     }
-
-    eta = norm(X, "2")^2 + norm(matrix(1,n,1),"2")^2+1
-    H = -t(X)%*%(X - X%*%Z - E + (1/mu)*Y1) - matrix(1,n,1)%*%(matrix(1,1,n)- matrix(1,1,n)%*%Z + (1/mu)*Y2) + (Z - J + (1/mu)*Y3)
-    Z = Z - (1/eta)*H
-    Z[D>0] = 0
+    
+    eta <- norm(X, "2")^2 + norm(matrix(1,n,1),"2")^2+1
+    H <- -t(X)%*%(XXZ-E + (1/mu)*Y1) - matrix(1,n,1)%*%(matrix(1,1,n)- matrix(1,1,n)%*%Z + (1/mu)*Y2) + (Z - J + (1/mu)*Y3)
+    Z <- Z - (1/eta)*H
+    Z[D>0] <- 0
+    XXZ <- X-X%*%Z
+    
+    
     # Update Dual variable
-    Y1 = Y1 + mu*(X-X%*%Z-E)
-    Y2 = Y2 + mu*(matrix(1,1,n) - matrix(1,1,n)%*%Z)
-    Y3 = Y3 + mu*(Z - J)
-
-    Err[iter,] = c(norm(X-X%*%Z-E,"2"), norm(Z-J,"2"))
-
+    Y1 <- Y1 + mu*(XXZ-E)
+    Y2 <- Y2 + mu*(matrix(1,1,n) - matrix(1,1,n)%*%Z)
+    Y3 <- Y3 + mu*(Z - J)
+    
+    Err[iter,] <- c(norm(XXZ-E,"2"), norm(Z-J,"2"))
+    print(paste0("Err = ", Err[iter,1]))
     if (max(Err[iter,]) <= epsilon){
       print("reached epsilon")
-      return(list(Z = Z, E = norm(X-X%*%Z,"f")))
+      return(list(Z = Z, E = norm(XXZ,"f")))
     }
- }
+  }
 }
--- tests/testthat/testL2R2.R
+++ tests/testthat/testL2R2.R
@@ -2,7 +2,12 @@ context("similarity matrix based on L2R2 manifold embedding")
 library(RSoptSC)
 
 test_that("norm(X-X%*%Z,\"f\") is less than or equal to matlab", {
-  # this is a placeholder test.
-  W <- SimilarityM(lambda = JoostL2R2$lambda, data = JoostL2R2$data)
+  
+  W <- SimilarityM(lambda = JoostL2R2$lambda, 
+                   data = JoostL2R2$data,
+                   perplexity = 25, 
+                   dims = 3, 
+                   pca = TRUE,
+                   theta = 0)
   expect_true(W$E <= JoostL2R2$FNorm_XXZ_error)
 })
