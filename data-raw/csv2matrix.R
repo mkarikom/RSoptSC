d1250c83cc3eb5fe0132d882ad2f676f85165ffd
Matt Karikomi <mkarikom@users.noreply.github.com>
2019-03-18 10:53:21 -0700

    Delete csv2matrix.R

--- data-raw/csv2matrix.R
+++
@@ -1,7 +0,0 @@
-csv2matrix <- function(filename,
-                       foldername = "testdata", thispackage = "RSoptSC" ){
-  pathstring <- system.file(foldername,filename,thispackage)
-  dframe <- read.csv(pathstring)
-  dmatrix <- as.matrix(dframe[-1,])
-  return(dmatrix)
-}
