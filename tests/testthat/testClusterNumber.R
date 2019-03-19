6ed22bf9c9936d0208669d089bcdcb135846d26f
Matt Karikomi <mattkarikomi@gmail.com>
2019-03-18 23:53:24 -0700

    removed old marker gene test

--- tests/testthat/testClusterNumber.R
+++ tests/testthat/testClusterNumber.R
@@ -2,6 +2,6 @@ context("cluster number inference on Joost data")
 library(RSoptSC)
 
 test_that("initial components are correct", {
-  comps <- GetComponents(data = sparseJoostTest$S, tol = 0.01)
+  comps <- GetComponents(data = joostTest$S, tol = 0.01)
   expect_equal(comps$n_eigs, 19)
 })
--- tests/testthat/testL2R2.R
+++ tests/testthat/testL2R2.R
@@ -2,7 +2,7 @@ context("ADMM result")
 library(RSoptSC)
 
 test_that("norm(X-X%*%Z,\"f\") is less than or equal to matlab", {
-  data <- Matrix::as.matrix(sparseJoostTest$data)
+  data <- Matrix::as.matrix(joostTest$data)
   X <- data - min(data)
   X <- X / max(X)
   
@@ -10,6 +10,6 @@ test_that("norm(X-X%*%Z,\"f\") is less than or equal to matlab", {
     X[,i] <- X[,i] / norm(X[,i], "2")
   }
   # computes the similarity matrix using a low rank representation of X subject to the adjacency constraint D
-  M <- computeM(lambda = 0.5, X = X, D = Matrix::as.matrix(sparseJoostTest$D))
+  M <- computeM(lambda = 0.5, X = X, D = Matrix::as.matrix(joostTest$D))
   expect_true(abs(M$E-16.7979) <= 1e-5)
 })
