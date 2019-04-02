d9dd77525c97022824aec25406e249ec90665612
Matt Karikomi <mattkarikomi@gmail.com>
2019-04-01 17:23:34 -0700

    removed implicit data selection from find markers, still log normalized

--- R/MarkerGenes.R
+++ R/MarkerGenes.R
@@ -2,12 +2,12 @@
 #' 
 #' Get the marker genes for each cluster and report their 1-normalized expression value.
 #'
-#' @param counts_data a matrix of expression values for each cell (rows) and gene (columns)
+#' @param counts_data a matrix of expression values for each cell (rows) and gene (columns) which will be normalized and 
 #' @param cluster_labels a vector of cluster labels
 #' @param H a nonnegative matrix such that W = H*t(H), H_{i,j} is the cells_weight
 #'     by which cell i belongs to the jth cluster
 #' @param n_sorted the number of markers to report in the n_sorted table
-#' @param gene_names gene symbols corresponding to rows in counts_data
+#' @param gene_names gene symbols corresponding to rows in counts_data (this should match rows of M, if provided)
 #' @param gene_expression_threshold for n cells, for \code{gene_expression_threshold} = m, dont consider genes
 #'     expressed in more than n-m cells or genes expressed in less than m cells
 #' @param n_features number of marker genes per cluster to retrieve
@@ -16,36 +16,33 @@
 #'     \item{all}{all the markers listed by gene index, cluster, and score}
 #'     \item{n_sorted}{a subset of the sorted marker table with gene symbols}
 #'
+#' @importFrom dplyr arrange
+#'
 #' @export
 #'
 GetMarkerTable <- function(counts_data,
-                           cluster_labels,
-                           H,
-                           n_sorted = 50,
-                           gene_names,
-                           gene_expression_threshold,
-                           n_features){
+                            cluster_labels,
+                            H,
+                            n_sorted = 50,
+                            gene_names,
+                            gene_expression_threshold,
+                            n_features){
   # filter the data according to desired samples (cells) and features (genes)
+  browser()
   clusterId = geneScore = NULL # get r cmd check to pass
-  M <- SelectData(counts_data,
-                  gene_expression_threshold,
-                  n_features)
-
-  # normalize the expression cell by cell
-  Mnorm <- apply(M$M_variable, 2, function(x){
-    x / sum(x)
-  })
-
+  M <- counts_data
+  Mnorm <- LogNormalize(M$M_variable)
+  
   # for each gene (rows), for each cluster (columns)
   # get cell-weighted expression score
   gene_cluster_scores <- Mnorm %*% H
-
+  
   # select max cluster score
   gene_assignments <- apply(gene_cluster_scores, 1, function(x){
     c(which(x == max(x)), max(x))
   })
-
-
+  
+  
   marker_table <- cbind(M$gene_use, t(gene_assignments))
   colnames(marker_table) <- c('geneID', 'clusterId', 'geneScore')
   
